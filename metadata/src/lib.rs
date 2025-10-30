861950934f98a2a1566f82e7e09e8aac07d2df3e
Eizeed <eizeed321@gmail.com>
2025-10-30 13:57:18 +0500

    feat(metdata crate): init metadata crate

---
+++ metadata/src/lib.rs
@@ -0,0 +1,91 @@
+use acoustid_api::response::OkResponse;
+use acoustid_api::response::Recording;
+use acoustid_api::response::ReleaseType;
+
+pub enum Error {
+    InvalidMetadata,
+}
+
+#[derive(Debug)]
+pub struct Metadata {
+    pub artist_names: Vec<String>,
+    pub release_type: ReleaseType,
+    pub track_title: String,
+    pub release_title: String,
+    pub release_artists: Vec<String>,
+}
+
+impl TryFrom<OkResponse> for Metadata {
+    type Error = Error;
+
+    fn try_from(value: OkResponse) -> Result<Metadata, Error> {
+        let mut artist_names: Option<Vec<String>> = None;
+        let mut release_type: Option<ReleaseType> = None;
+        let mut track_title: Option<String> = None;
+        let mut release_title: Option<String> = None;
+        let mut release_artists: Option<Vec<String>> = None;
+
+        let res_recordings = value.results.into_iter().map(|mut r| {
+            r.recordings = r
+                .recordings
+                .into_iter()
+                .filter(|rec| {
+                    rec.artists.is_some() && rec.releasegroups.is_some() && rec.title.is_some()
+                })
+                .map(|mut recording| {
+                    recording.releasegroups = recording.releasegroups.map(|rg| {
+                        rg.into_iter()
+                            .filter(|rg| {
+                                let r = rg.secondary_types.as_ref().map(|t| {
+                                    t.into_iter()
+                                        .map(|s| (*s).as_str())
+                                        .find(|s| *s == "Remix")
+                                        .is_some()
+                                });
+                                if let Some(r) = r { r } else { false }
+                            })
+                            .collect()
+                    });
+                    recording
+                })
+                .collect::<Vec<Recording>>();
+            r
+        });
+
+        for res in res_recordings {
+            for rec in res.recordings {
+                if artist_names.is_none() {
+                    artist_names = Some(rec.artists.unwrap().into_iter().map(|a| a.name).collect());
+                }
+                if track_title.is_none() {
+                    track_title = Some(rec.title.unwrap());
+                }
+
+                for rg in rec.releasegroups.unwrap() {
+                    if let Some(rt) = release_type.as_ref() {
+                        if matches!(rt, ReleaseType::Album) || matches!(rt, ReleaseType::Ep) {
+                            continue;
+                        } else {
+                            release_type = Some(rg.release_type);
+                            release_title = Some(rg.title);
+                            release_artists =
+                                Some(rg.artists.into_iter().map(|a| a.name).collect());
+                        }
+                    } else {
+                        release_type = Some(rg.release_type);
+                        release_title = Some(rg.title);
+                        release_artists = Some(rg.artists.into_iter().map(|a| a.name).collect());
+                    }
+                }
+            }
+        }
+
+        Ok(Metadata {
+            artist_names: artist_names.ok_or(Error::InvalidMetadata)?,
+            release_type: release_type.ok_or(Error::InvalidMetadata)?,
+            track_title: track_title.ok_or(Error::InvalidMetadata)?,
+            release_title: release_title.ok_or(Error::InvalidMetadata)?,
+            release_artists: release_artists.ok_or(Error::InvalidMetadata)?,
+        })
+    }
+}
